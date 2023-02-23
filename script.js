32c262cf8d7eaeb68d1be3afd451dcea4aa88186
limteractive <limteractive@gmail.com>
2023-02-23 13:47:33 +0900

    Refactoring

--- script.js
+++ script.js
@@ -86,13 +86,13 @@ window.addEventListener('load', () => {
     loading.style.display = 'none';
 
     // cover interaction
-    // window.addEventListener('scroll', (e) => {
-    //     let val = window.scrollY;
-    //     cirs[0].style.top = 150 + val * -0.5 + 'px';
-    //     cirs[1].style.bottom = 100 + val * -0.2 + 'px';
-    //     cirs[2].style.top = 200 + val * -0.2 + 'px';
-    //     cirs[3].style.top = 100 + val * -0.2 + 'px';
-    // })
+    window.addEventListener('scroll', (e) => {
+        let val = window.scrollY;
+        cirs[0].style.top = 150 + val * -0.5 + 'px';
+        cirs[1].style.bottom = 100 + val * -0.08 + 'px';
+        cirs[2].style.top = 200 + val * -0.2 + 'px';
+        cirs[3].style.top = 100 + val * -0.2 + 'px';
+    })
 
     // 업종 분석 결과
     codes.forEach((elem, idx) => {
